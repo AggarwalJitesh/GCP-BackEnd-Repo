08d8c873accbf4dd062e98cebacaea3f8af18d6c
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2023-11-17 23:29:52 +0530

    502 removal attempt 15

--- main.py
+++ main.py
@@ -80,3 +80,6 @@ def upload_image():
 #     # app.run(debug=True)
 
 #     app.run(port=int(os.environ.get("PORT",5000)),host='0.0.0.0',debug=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
