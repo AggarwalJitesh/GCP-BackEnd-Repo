bd444e5fe123dbe7278373da3b8c6ab8c93adb22
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2023-11-18 22:19:23 +0530

    502 removal attempt 21

--- main.py
+++ main.py
@@ -67,6 +67,6 @@ async def classify_image(file: UploadFile = File(...)):
 # Run the FastAPI app with Uvicorn
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
     # uvicorn.run(app, host="0.0.0.0", port=5000)
