1eca19cceeed424595583b6c3d495e972f726265
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2023-11-20 22:56:57 +0530

    502 removal attempt 22

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import io
 import os
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from flask import Flask, send_from_directory
@@ -24,27 +24,28 @@ app.add_middleware(
 model = tf.keras.models.load_model('model.h5')
 
 # Create a Flask app for serving the React app
-react_app = Flask(__name__, static_folder="react_app/build", static_url_path="/")
+# react_app = Flask(__name__, static_folder="react_app/build", static_url_path="/")
 
-# Route to serve the React app
-@react_app.route("/")
-def index():
-    return send_from_directory(react_app.static_folder, "index.html")
+# # Route to serve the React app
+# @react_app.route("/")
+# def index():
+#     return send_from_directory(react_app.static_folder, "index.html")
 
-# Route to serve static files from the React app
-@react_app.route("/static/<path:filename>")
-def static_files(filename):
-    return send_from_directory(react_app.static_folder + "/static", filename)
+# # Route to serve static files from the React app
+# @react_app.route("/static/<path:filename>")
+# def static_files(filename):
+#     return send_from_directory(react_app.static_folder + "/static", filename)
+
 
 # Route to handle image classification
 @app.post("/classify")
-async def classify_image(file: UploadFile = File(...)):
+async def classify_image(image: UploadFile = File(...)):
     # Read the image file
-    contents = await file.read()
+    contents = await image.read()
     image = Image.open(io.BytesIO(contents))
 
     # Preprocess the image
-    image = image.resize((224, 224))  # Adjust the size based on your model requirements
+    image = image.resize((150, 150))  # Adjust the size based on your model requirements
     image_array = np.array(image) / 255.0
     image_array = np.expand_dims(image_array, axis=0)
 
@@ -62,7 +63,7 @@ async def classify_image(file: UploadFile = File(...)):
     class_name = class_labels[predicted_class]
 
     # Return the result as JSON
-    return JSONResponse(content={"class": class_name})
+    return JSONResponse({'message': str(class_name)})
 
 # Run the FastAPI app with Uvicorn
 if __name__ == "__main__":
