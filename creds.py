17a65f557a5740d1dffacc7d764e9eecb552427d
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2024-02-09 11:01:11 +0530

    2nd database added

---
+++ creds.py
@@ -0,0 +1,7 @@
+HTTPlink = 'https://nd-651-483-575.p2pify.com/cbda8d1c04f6e11e5f15b7a9cb95183f'
+
+contract_address = '0x96DE750De9C3AB4b5916FbbF977AE6FE2Fc0f739'
+
+account_address = '0x765941e3AA25533001280b2e0463a5544b165d0F'
+
+private_key = '0xa45e9945d494d99189a78ffe74bdc21b362276d86a8b920c59a2d89c40ad9ecc'
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from databases import Database
 
 # GCP cloud storage
 from google.cloud import storage
+from datetime import datetime
 
 # static file save
 from fastapi.staticfiles import StaticFiles
@@ -24,6 +25,10 @@ import json
 from eth_abi.exceptions import EncodingError
 from hexbytes import HexBytes
 
+# connectiion with creds file
+from creds import HTTPlink, contract_address, account_address, private_key
+
+
 app = FastAPI()
 
 
@@ -31,7 +36,7 @@ app = FastAPI()
 IMAGE_DIR = "static"
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# test variable
+userid = ""
 imgurl = ""
 classificationResult = ""
 unique_filename = ""
@@ -96,10 +101,6 @@ async def classify_image(image: UploadFile = File(...)):
     return JSONResponse({'message': str(class_name)})
 
 
-# GCP cloud sql commands
-DATABASE_URL = "mysql://root:blockconvey2024@34.29.182.200:3306/Signup"
-
-
 class FormData(BaseModel):
     username: str
     email: str
@@ -110,6 +111,9 @@ class loginFormData(BaseModel):
     email: str
     password: str
 
+
+# GCP cloud sql commands
+DATABASE_URL = "mysql://root:blockconvey2024@34.29.182.200:3306/Signup"
 
 database = Database(DATABASE_URL)
 
@@ -128,6 +132,12 @@ async def verify_password(email: str, user_password: str) -> bool:
         return user_password == stored_password_hash
     else:
         return False
+
+
+async def finduser(email: str) -> str:
+    query = "SELECT username FROM users WHERE email = :email"
+    result = await database.fetch_one(query, {"email": email})
+    return result['username']
 
 
 @app.post("/submit-form")
@@ -154,23 +164,26 @@ async def login(data: loginFormData):
     await database.connect()
 
     if await email_exists(data.email) and await verify_password(data.email, data.password):
-        return {"message": "User Authenticated"}
+        global userid
+        userid = await finduser(data.email)
+        return {"message": "User Authenticated", "userid": userid}
+    else:
+        return {"message": "Authentication Failed"}
+
+
+@app.get("/logout")
+async def logout():
+    userid = None
+    print("logout = ", userid)
+    return {"message": "Logged out successfully"}
 
 
 # BLOCKCHAIN CONNECTION
 
-w3 = Web3(Web3.HTTPProvider(
-    'https://nd-651-483-575.p2pify.com/cbda8d1c04f6e11e5f15b7a9cb95183f'))
-
-contract_address = '0x96DE750De9C3AB4b5916FbbF977AE6FE2Fc0f739'
-
+w3 = Web3(Web3.HTTPProvider(HTTPlink))
 
 with open('NFT.json', 'r') as abi_file:
     contract_abi = json.load(abi_file)
-
-account_address = '0x765941e3AA25533001280b2e0463a5544b165d0F'
-private_key = '0xa45e9945d494d99189a78ffe74bdc21b362276d86a8b920c59a2d89c40ad9ecc'
-
 
 
 def hexbytes_to_hex(json_data):
@@ -186,21 +199,10 @@ def hexbytes_to_hex(json_data):
     return json_data
 
 
-@app.get("/transaction")
-async def transaction_dict():
+# GCP cloud sql commands
+DATABASE_URL_uploadhistory = "mysql://root:blockconvey2024@34.29.182.200:3306/uploadHistory"
 
-    json_serializable_dict = hexbytes_to_hex(original_dict)
-
-    data_to_send = {
-        "blockHash": json_serializable_dict["blockHash"],
-        "blockNumber": json_serializable_dict["blockNumber"],
-        "hash": json_serializable_dict["hash"],
-        "imageName": unique_filename,
-        "result": classificationResult,
-        "url": imgurl
-    }
-
-    return data_to_send
+database_uploadhistory = Database(DATABASE_URL_uploadhistory)
 
 
 @app.get("/addtoblockchain")
@@ -219,42 +221,57 @@ async def addtochain():
 
         })
 
-        # Sign the transaction
         signed_tx = w3.eth.account.sign_transaction(
             create_token_txn, private_key=private_key)
         # print("signed_txn = ", signed_txn)
 
-        # Send the transaction
         tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
         tx_hash_hex = tx_hash.hex()
         print("tx_hash = ", tx_hash_hex)
 
-        # Get transaction receipt
         tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
         print("tx_receipt = ", tx_receipt)
 
         global original_dict
         original_dict = dict(w3.eth.get_transaction(tx_hash))
-        
-        return {"message": "added successfully"}
+
+        json_serializable_dict = hexbytes_to_hex(original_dict)
+
+        current_datetime = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+        # saving image perticular in gcp
+
+        await database_uploadhistory.connect()
+
+        query = "INSERT INTO uploadLogs(imagename, result, blockHash , blockNumber , txnHash , imgUrl, dateTime, userid ) VALUES (:imagename, :result, :blockHash , :blockNumber , :txnHash , :imgUrl, :dateTime, :userid )"
+        values = {"imagename": unique_filename, "result": classificationResult,
+                  "blockHash": json_serializable_dict["hash"], "blockNumber": json_serializable_dict["blockNumber"], "txnHash": tx_hash_hex, "imgUrl": imgurl, "dateTime": current_datetime, "userid": userid}
+
+        try:
+            await database_uploadhistory.execute(query, values)
+            return {"message": "added successfully"}
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
 
     except EncodingError:
         raise HTTPException(status_code=400, detail="Invalid image URL format")
 
-    # Parse the transaction receipt to find the events
 
-    # if tx_receipt.status == 1 and tx_receipt.logs:
-    #     try:
-    #         decoded_logs = contract.events.Transfer().processReceipt(tx_receipt)
-    #         for event in decoded_logs:
-    #             token_id = event.args.tokenId
-    #             print("Token ID:", token_id)
-    #     except Exception as e:
-    #         print("Error processing receipt:", e)
-    # else:
-    #     print("No successful transfer events found or transaction failed.")
+@app.get("/transaction")
+async def transaction_dict():
+    
+    await database_uploadhistory.connect()
+
+    query = "SELECT imagename, result, blockHash , blockNumber , txnHash , imgUrl, dateTime FROM uploadLogs WHERE userid = :userid"
+    result = await database_uploadhistory.fetch_all(query, {"userid": userid})
+    
+    if result:
+        return [dict(row) for row in result]
+
 
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
